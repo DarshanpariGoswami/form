cf9efaa1a87b4d88620b20e1764d22df8431ee55
DarshanpariGoswami <124126426+DarshanpariGoswami@users.noreply.github.com>
2023-03-10 18:14:30 +0530

    Add files via upload

---
+++ task-2.js
@@ -0,0 +1,8 @@
+var submit = document.getElementById("submitbutton");
+var table = document.getElementsByClassName("myDIV");
+//submit.addEventListener("click", tableeve);
+function tableeve() {
+  //table.style.display = "none";
+  document.getElementsByClassName(".myDIV").style.scale = "1";
+}
+document.getElementsByClassName("myDIV").style.scale = "1";
